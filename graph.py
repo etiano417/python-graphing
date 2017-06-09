b027b3b120ad97a7cb78db58f187c75a2fa57e78
Eric Tiano <etiano417@gmail.com>
2017-06-09 15:00:13 -0400

    Added validation in Graph constructor
     - added tests for constructor parameter types
     - added tests for constructor parameter values
     - removed edge Set from Graph (as it is fully captured in the key Set of the incidence Dictionary

--- graph.py
+++ graph.py
@@ -1,3 +1,4 @@
+from collections import Counter
 """Graphs and graph functions.
 
 This module offers a collection of graph classes and graph-related functions. These classes and functions are intended
@@ -10,22 +11,39 @@ Classes:
 class Graph:
     """A set of edges mapped onto a set of vertices with an incidence map
 
-    This represents a set of vertices, a set of edges, and an incidence map that connects the two. The sets of vertices
-    and edges are disjoint. The incidence map only includes edges as keys, and it only includes 2 element Counters
+    This represents a set of vertices, and an incidence map that maps edges to vertices. The sets of vertices
+    and edges are disjoint. The incidence map's keys are edges, and it only includes 2 element Counters
     (representing unordered pairs) as values. These Counters include only vertices.
     """
 
-    def __init__(self, vertices = set(), edges = set(), incidence = dict()):
+    def __init__(self, vertices = set(), incidence = dict()):
+        if not type(vertices) is set:
+            raise TypeError("vertices must be a Set")
+        if not type(incidence) is dict:
+            raise TypeError("incidence must be a Dictionary")
+        for e in incidence.values():
+            if not e is Counter:
+                raise TypeError("incidence's values must all be Counters")
+
+        if not vertices.isdisjoint(incidence.keys):
+            raise ValueError("vertices and edges must be disjoint")
+        #make sure each counter contains a total of 2
+        for e in incidence.values():
+            if sum(e.values) != 2:
+                raise ValueError("each edge in incidence needs exactly two end vertices")
+
         self._vertices = vertices
         self._edges = edges
-        self._incidence = incidence
 
+    @property
     def vertices(self):
         return self._vertices
 
+    @property
     def edges(self):
-        return self._edges
+        return self._incidence.keys
 
+    @property
     def incidence(self):
         return self._incidence
 
